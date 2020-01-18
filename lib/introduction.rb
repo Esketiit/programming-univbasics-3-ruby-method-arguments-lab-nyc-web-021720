099f1969cdec6a162028f8230c610b47680dd8f4
Brian Desplantes <heftygac@gmail.com>
2020-01-18 20:22:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,13 @@ def introduction (name)
   puts "Hello, my name is #{name}."
 end
 
+def introduction_with_language (name, language)
+  puts "Hello, , my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language_optional (name, language = ruby)
+  puts "Hello, , my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
